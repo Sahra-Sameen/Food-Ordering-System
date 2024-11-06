e93b5e4f47157948392eff94ef00ace1d9e0d803
SAHRA M.S.F. <126109790+Sahra-Sameen@users.noreply.github.com>
2024-11-06 10:35:42 +0530

    Add to cart functionality added

--- foodMine/src/app/Services/food/food.service.ts
+++ foodMine/src/app/Services/food/food.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Food } from '../../shared/models/Food';
+import { Tag } from '../../shared/models/Tag';
 
 @Injectable({
   providedIn: 'root',
@@ -7,118 +8,146 @@ import { Food } from '../../shared/models/Food';
 export class FoodService {
   constructor() {}
 
+  getFoodById(id: number): Food{
+    return this.getAll().find(food => food.id == id)!;
+  }
+
+  getAllFoodsBySearchTerm(searchTerm: string): Food[]{
+    return this.getAll().filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  }
+
+  getAllTags(): Tag[]{
+    return [
+      { name: 'All', count: 10 },
+      { name: 'FastFood', count: 4 },
+      { name: 'Pizza', count: 1 },
+      { name: 'Lunch', count: 6 },
+      { name: 'SlowFood', count: 4 },
+      { name: 'Hamburger', count: 1 },
+      { name: 'Fry', count: 2 },
+      { name: 'Soup', count: 2 }
+    ]
+  }
+
+  getAllFoodsByTag(tag: string): Food[] {
+    return tag.toLowerCase() === 'all' ? 
+      this.getAll() :
+      this.getAll().filter((food) => food.tags?.some(foodTag => foodTag.toLowerCase() === tag.toLowerCase()));
+  }
+  
+
   getAll(): Food[] {
     return [
       {
-        id:1,
-        name: "Burger",
+        id: 1,
+        name: 'Burger',
         price: 95,
         favorite: false,
         stars: 3,
         imageUrl: '/assets/images/food-1.jpg',
         origins: ['India', 'USA'],
         cookTime: '20-30',
-        tags: ['Fast food', 'Mexican']
+        tags: ['FastFood', 'Hamburger'],
       },
       {
-        id:2,
-        name: "Noodles",
+        id: 2,
+        name: 'Noodles',
         price: 150,
         favorite: true,
         stars: 5,
         imageUrl: '/assets/images/food-2.jpg',
         origins: ['India', 'USA'],
         cookTime: '20-30',
-        tags: ['Fast food', 'Mexican']
+        tags: ['Lunch', 'SlowFood'],
       },
       {
-        id:3,
-        name: "Maccaroni",
+        id: 3,
+        name: 'Maccaroni',
         price: 450,
         favorite: true,
         stars: 2,
         imageUrl: '/assets/images/food-3.jpg',
         origins: ['India', 'USA'],
         cookTime: '20-30',
-        tags: ['Fast food', 'Mexican']
+        tags: ['Slowfood', 'Lunch'],
       },
       {
-        id:4,
-        name: "Pizza",
+        id: 4,
+        name: 'Pizza',
         price: 950,
         favorite: false,
         stars: 4,
         imageUrl: '/assets/images/food-4.jpg',
         origins: ['India', 'USA'],
         cookTime: '20-30',
-        tags: ['Fast food', 'Mexican']
+        tags: ['FastFood', 'Pizza'],
       },
       {
-        id:5,
-        name: "Sea Food Noodles",
+        id: 5,
+        name: 'Sea Food Noodles',
         price: 750,
         favorite: false,
         stars: 2,
         imageUrl: '/assets/images/food-5.jpg',
         origins: ['India', 'USA'],
         cookTime: '20-30',
-        tags: ['Fast food', 'Mexican']
+        tags: ['Slowfood', 'Lunch'],
       },
       {
-        id:6,
-        name: "Vegetable Rice",
+        id: 6,
+        name: 'Vegetable Rice',
         price: 950,
         favorite: true,
         stars: 5,
         imageUrl: '/assets/images/food-6.jpg',
         origins: ['India', 'USA'],
         cookTime: '20-30',
-        tags: ['Fast food', 'Mexican']
+        tags: ['Lunch', 'Slowfood'],
       },
       {
-        id:7,
-        name: "Soup",
+        id: 7,
+        name: 'Soup',
         price: 1050,
         favorite: false,
         stars: 5,
         imageUrl: '/assets/images/food-7.jpg',
         origins: ['India', 'USA'],
         cookTime: '20-30',
-        tags: ['Fast food', 'Mexican']
+        tags: ['Soup', 'Lunch'],
       },
       {
-        id:8,
-        name: "Salad",
+        id: 8,
+        name: 'Salad',
         price: 950,
         favorite: true,
         stars: 3,
         imageUrl: '/assets/images/food-8.jpg',
         origins: ['India', 'USA'],
         cookTime: '20-30',
-        tags: ['Fast food', 'Mexican']
+        tags: ['Soup', 'Lunch'],
       },
       {
-        id:9,
-        name: "Fish Fry",
+        id: 9,
+        name: 'Fish Fry',
         price: 950,
         favorite: true,
         stars: 3,
         imageUrl: '/assets/images/food-9.jpeg',
         origins: ['India', 'USA'],
         cookTime: '20-30',
-        tags: ['Fast food', 'Mexican']
+        tags: ['Fry', 'FastFood'],
       },
       {
-        id:10,
-        name: "Chicken Fry",
+        id: 10,
+        name: 'Chicken Fry',
         price: 250,
         favorite: false,
         stars: 1,
         imageUrl: '/assets/images/food-10.jpeg',
         origins: ['India', 'USA'],
         cookTime: '20-30',
-        tags: ['Fast food', 'Mexican']
-      }
+        tags: ['Fry', 'FastFood'],
+      },
     ];
   }
 }
